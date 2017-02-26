af714669d1dd5cb73e4b8d5567b9cecc95b87af1
Rakshasa <orgalvin@gmail.com>
2017-02-26 16:49:52 -0500

    added basic key pair functionality

--- src/RSAKeyPair.java
+++ src/RSAKeyPair.java
@@ -1,4 +1,30 @@
+import java.math.*;
 
 public class RSAKeyPair {
+	private String privateKey;
+	private String publicKey;
+	
+	RSAKeyPair() {
+		generateKeyPair();
+	}
+	
+	private void generateKeyPair() {
+		BigInteger p = generatePrime();
+		BigInteger q = generatePrime();
+		
+	}
+	
+	private BigInteger generatePrime() {
+		return null;
+	}
+	
+	public String getPrivateKey() {
+		return null;
+		
+	}
+	
+	public String getPublicKey() {
+		return null;
+	}
 
 }
